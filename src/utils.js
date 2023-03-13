a535624cbe39e4eed9f02e96e4271e04c2b0edd5
MrBoombastic <contact@amroz.xyz>
2023-03-13 18:57:02 +0100

    dep bump, unwanted nulls removed

--- src/utils.js
+++ src/utils.js
@@ -3,8 +3,8 @@ const packagejson = require("../package.json");
 
 module.exports = {
     formatUserData: function (member, spacing, dateFormat) {
-        const data = [member.id, member.user.tag, member?.nickname || "NULL", member.user?.avatar || "NULL", member.roles.cache.map(role => `${role.id} - ${role.name}`).join(", "),
-            dayjs(member.user.createdAt).format(dateFormat), dayjs(member.joinedAt).format(dateFormat), member?.presence?.activities?.map(activity => activity?.name).join(", ") || "NULL",
+        const data = [member.id, member.user.tag, member?.nickname || "", member.user?.avatar || "", member.roles.cache.map(role => `${role.id} - ${role.name}`).join(", "),
+            dayjs(member.user.createdAt).format(dateFormat), dayjs(member.joinedAt).format(dateFormat), member?.presence?.activities?.map(activity => activity?.name).join(", ") || "",
             member?.presence?.status.toUpperCase(), member.user?.displayAvatarURL({
                 size: 1024,
                 dynamic: true
